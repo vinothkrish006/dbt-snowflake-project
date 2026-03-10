80b7113acae5c87340921750223d1dd8680919b0
vinoth raja <rajavinothkumar06@gmail.com>
2026-03-10 14:58:36 +0000

    committed

---
+++ models/staging/snowflake_raw/stg_snowflake_raw__employees.sql
@@ -0,0 +1,28 @@
+with 
+
+source as (
+
+    select * from {{ source('snowflake_raw', 'employees') }}
+
+),
+
+renamed as (
+
+    select
+        employee_id,
+        first_name,
+        last_name,
+        email,
+        phone_number,
+        hire_date,
+        job_id,
+        salary,
+        commission_pct,
+        manager_id,
+        department_id
+
+    from source
+
+)
+
+select * from renamed
