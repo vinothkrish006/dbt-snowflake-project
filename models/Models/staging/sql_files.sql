89660b748bcd9f3456d5f60061bf1a2a1a4cb938
vinoth raja <rajavinothkumar06@gmail.com>
2026-03-10 17:11:06 +0000

    commited

--- models/Models/staging/sql_files.sql
+++ models/Models/staging/sql_files.sql
@@ -1,11 +1,9 @@
-{{ config (materialized='view')}}
+{{ config(materialized='view') }}
 
+with source as (
 
-with 
-
-source as (
-
-    select * from {{ source('snowflake_raw', 'employees') }}
+    select * 
+    from {{ source('snowflake_raw','employees') }}
 
 ),
 
